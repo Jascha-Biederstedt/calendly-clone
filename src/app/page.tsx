142659a314ae6575e87b309c3c7d73332a13bdd8
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-10-15 18:31:49 +0200

    Add sign in and sign up btns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return null;
+  return (
+    <div className='text-center container my-4 mx-auto'>
+      <h1 className='text-3xl mb-4'>Calendly Clone</h1>
+      <div className='flex gap-2 justify-center'>
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
+      </div>
+    </div>
+  );
 }
