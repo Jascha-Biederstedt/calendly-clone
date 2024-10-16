572b1d96cca497790f7e22aecaf4b2a9b0c3f2e1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-10-16 14:13:35 +0200

    Add event card

---
+++ src/app/(private)/events/new/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+const NewEventPage = () => {
+  return (
+    <Card className='max-w-md max-auto'>
+      <CardHeader>
+        <CardTitle>New Event</CardTitle>
+      </CardHeader>
+      <CardContent>content</CardContent>
+    </Card>
+  );
+};
+
+export default NewEventPage;
