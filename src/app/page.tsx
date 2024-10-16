985356321d6474f6b2d5c47a26cb2fb0b19b938e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-10-16 10:53:08 +0200

    Add clerk functionality for sign in and sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { Button } from '@/components/ui/button';
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div className='text-center container my-4 mx-auto'>
       <h1 className='text-3xl mb-4'>Calendly Clone</h1>
       <div className='flex gap-2 justify-center'>
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+        <UserButton />
       </div>
     </div>
   );
