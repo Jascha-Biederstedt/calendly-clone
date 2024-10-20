cd44de35d18f4adcf0c10ffeb852cc28d27eda80
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-10-20 12:59:43 +0200

    Add schedule form schema

--- src/app/(private)/schedule/page.tsx
+++ src/app/(private)/schedule/page.tsx
@@ -1,3 +1,4 @@
+import ScheduleForm from '@/components/forms/ScheduleForm';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { db } from '@/drizzle/db';
 import { auth } from '@clerk/nextjs/server';
---
+++ src/components/forms/ScheduleForm.tsx
@@ -0,0 +1,200 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormMessage,
+} from '../ui/form';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+  AlertDialogTitle,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from '../ui/alert-dialog';
+import { Switch } from '../ui/switch';
+import { Input } from '../ui/input';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+
+import { Textarea } from '../ui/textarea';
+import { createEvent, updateEvent, deleteEvent } from '@/server/actions/events';
+import { DAYS_OF_WEEK_IN_ORDER } from '@/data/constants';
+import { scheduleFormSchema } from '@/schema/schedule';
+
+type Availability = {
+  startTime: string;
+  endTime: string;
+  dayOfWeek: (typeof DAYS_OF_WEEK_IN_ORDER)[number];
+};
+
+const ScheduleForm = ({
+  schedule,
+}: {
+  schedule?: {
+    timezone: string;
+    availabilities: Availability[];
+  };
+}) => {
+  const form = useForm<z.infer<typeof scheduleFormSchema>>({
+    resolver: zodResolver(scheduleFormSchema),
+    defaultValues: event ?? {
+      isActive: true,
+      durationInMinutes: 30,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof scheduleFormSchema>) => {
+    const action =
+      event == null ? createEvent : updateEvent.bind(null, event.id);
+    const data = await action(values);
+
+    if (data?.error) {
+      form.setError('root', {
+        message: 'There was an error saving your event',
+      });
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex gap-6 flex-col'
+      >
+        {form.formState.errors.root && (
+          <div className='text-destructive text-sm'>
+            {form.formState.errors.root.message}
+          </div>
+        )}
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Event Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormDescription>
+                The name users will see when booking
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='durationInMinutes'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Duration</FormLabel>
+              <FormControl>
+                <Input type='number' {...field} />
+              </FormControl>
+              <FormDescription>In minutes</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='description'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea className='resize-none h-32' {...field} />
+              </FormControl>
+              <FormDescription>
+                Optional description of the event
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='isActive'
+          render={({ field }) => (
+            <FormItem>
+              <div className='flex items-center gap-2'>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel>Active</FormLabel>
+              </div>
+              <FormDescription>
+                Inactive events will not be visible for users to book
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className='flex gap-2 justify-end'>
+          {event && (
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  variant='destructiveGhost'
+                  disabled={isDeletePending || form.formState.isSubmitting}
+                >
+                  Delete
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    this event.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction
+                    disabled={isDeletePending || form.formState.isSubmitting}
+                    variant='destructive'
+                    onClick={() => {
+                      startDeleteTransition(async () => {
+                        const data = await deleteEvent(event.id);
+
+                        if (data?.error) {
+                          form.setError('root', {
+                            message: 'There was an error deleting this event',
+                          });
+                        }
+                      });
+                    }}
+                  >
+                    Delete
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          )}
+          <Button type='button' asChild variant='outline'>
+            <Link href='/events'>Cancel</Link>
+          </Button>
+          <Button type='submit'>Save</Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default ScheduleForm;
