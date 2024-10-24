85153102209c1c8ca15d0a61367f3a36029c4fa0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-10-24 11:56:07 +0200

    Implement getting valid times from schedule

--- src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
+++ src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
@@ -1,4 +1,27 @@
-const BookEventPage = () => {
+import { db } from '@/drizzle/db';
+import { clerkClient } from '@clerk/nextjs/server';
+import { addMonths, endOfDay, roundToNearestMinutes } from 'date-fns';
+import { notFound } from 'next/navigation';
+
+const BookEventPage = async ({
+  params: { clerkUserId, eventId },
+}: {
+  params: { clerkUserId: string; eventId: string };
+}) => {
+  const event = await db.query.EventTable.findFirst({
+    where: ({ clerkUserId: userIdCol, isActive, id }, { eq, and }) =>
+      and(eq(isActive, true), eq(userIdCol, clerkUserId), eq(id, eventId)),
+  });
+
+  if (event == null) return notFound();
+
+  const calendarUser = await clerkClient().users.getUser(clerkUserId);
+  const startDate = roundToNearestMinutes(new Date(), {
+    nearestTo: 15,
+    roundingMethod: 'ceil',
+  });
+  const endDate = endOfDay(addMonths(startDate, 2));
+
   return <h1>Hi</h1>;
 };
 
