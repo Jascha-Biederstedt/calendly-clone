4ca8d6f1d9ec07a6d1f46273fb9535c76567c220
Jascha-Biederstedt <j.biederstedt@gmx.de>
2024-10-24 12:22:13 +0200

    Add book event page

--- src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
+++ src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
@@ -1,7 +1,57 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { db } from '@/drizzle/db';
 import { clerkClient } from '@clerk/nextjs/server';
-import { addMonths, endOfDay, roundToNearestMinutes } from 'date-fns';
+import {
+  addMonths,
+  eachMinuteOfInterval,
+  endOfDay,
+  roundToNearestMinutes,
+} from 'date-fns';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+
+import { MeetingForm } from '@/components/forms/MeetingForm';
+import { getValidTimesFromSchedule } from '@/lib/getValidTimesFromSchedule';
+
+export const revalidate = 0;
+
+const NoTimeSlots = ({
+  event,
+  calendarUser,
+}: {
+  event: { name: string; description: string | null };
+  calendarUser: { id: string; fullName: string | null };
+}) => {
+  return (
+    <Card className='max-w-md mx-auto'>
+      <CardHeader>
+        <CardTitle>
+          Book {event.name} with {calendarUser.fullName}
+        </CardTitle>
+        {event.description && (
+          <CardDescription>{event.description}</CardDescription>
+        )}
+      </CardHeader>
+      <CardContent>
+        {calendarUser.fullName} is currently booked up. Please check back later
+        or choose a shorter event.
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href={`/book/${calendarUser.id}`}>Choose Another Event</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
 const BookEventPage = async ({
   params: { clerkUserId, eventId },
@@ -22,7 +72,34 @@ const BookEventPage = async ({
   });
   const endDate = endOfDay(addMonths(startDate, 2));
 
-  return <h1>Hi</h1>;
+  const validTimes = await getValidTimesFromSchedule(
+    eachMinuteOfInterval({ start: startDate, end: endDate }, { step: 15 }),
+    event
+  );
+
+  if (validTimes.length === 0) {
+    return <NoTimeSlots event={event} calendarUser={calendarUser} />;
+  }
+
+  return (
+    <Card className='max-w-4xl mx-auto'>
+      <CardHeader>
+        <CardTitle>
+          Book {event.name} with {calendarUser.fullName}
+        </CardTitle>
+        {event.description && (
+          <CardDescription>{event.description}</CardDescription>
+        )}
+      </CardHeader>
+      <CardContent>
+        <MeetingForm
+          validTimes={validTimes}
+          eventId={event.id}
+          clerkUserId={clerkUserId}
+        />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default BookEventPage;
